a8bd9026b081ff39547e232ae8be906e5249ece4
Panos Achlioptas <optas@stanford.edu>
2017-12-18 13:24:31 -0800

    mdb

--- download_data.sh
+++ download_data.sh
@@ -5,3 +5,5 @@ wget https://www.dropbox.com/s/vmsdrae6x5xws1v/shape_net_core_uniform_samples_20
 mv shape_net_core_uniform_samples_2048.zip\?dl\=0 shape_net_core_uniform_samples_2048.zip
 unzip shape_net_core_uniform_samples_2048.zip
 rm shape_net_core_uniform_samples_2048.zip
+mkdir data
+mv shape_net_core_uniform_samples_2048 data
